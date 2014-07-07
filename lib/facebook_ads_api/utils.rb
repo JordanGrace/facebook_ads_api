d69ed7498bdf928fe2aa0dccfdaf7bb3bbf8a0ad
Bill Watts <william.lane.watts@gmail.com>
2014-07-07 18:11:00 -0400

    Added url_unpack method that allows for a query string to be parsed and created into a hash.

--- lib/facebook_ads_api/utils.rb
+++ lib/facebook_ads_api/utils.rb
@@ -16,10 +16,40 @@ module FacebookAdsApi
 
     ##
     #
+    def url_unpack(str)
+      hash = {}
+
+      CGI::parse(str).to_a.map do |a|
+        if a.last.first.start_with?("[") and a.last.first.end_with?("]")
+          hash[a.first.to_sym] = a.last.first.split(",").map do |b|
+            if b.start_with?("[") or b.end_with?("]")
+              b = b.gsub("[","").gsub("]","")
+            end
+
+            b.gsub("\"","").gsub("\\","")
+          end
+        elsif a.last.first.is_i?
+          hash[a.first.to_sym] = a.last.first.to_i
+        else
+          hash[a.first.to_sym] = a.last.first
+        end
+      end
+
+      hash
+    end
+
+    ##
+    #
     def resourceify(name)
       name.to_s.split("_").map! do |s|
         [s[0,1].capitalize, s[1..-1]].join
       end.join
     end
+  end
+end
+
+class String
+  def is_i?
+    !!(self =~ /\A[-+]?[0-9]+\z/)
   end
 end
